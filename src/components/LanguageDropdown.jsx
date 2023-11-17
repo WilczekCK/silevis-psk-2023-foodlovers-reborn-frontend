d4415649c217e2ee901ee0714b9f88ee6919d7be
Pawel <wilkuwdr2008@gmail.com>
2023-11-18 00:44:05 +0100

    language dropdown works fine, added translations too

--- src/components/LanguageDropdown.jsx
+++ src/components/LanguageDropdown.jsx
@@ -1,35 +1,50 @@
 import { useTranslation } from 'react-i18next';
 import { DownOutlined, SmileOutlined } from '@ant-design/icons';
 import { Dropdown, Space } from 'antd';
+import { Image } from 'antd';
 
-const items = [
-    {
-      key: '1',
-      label: (
-        <a>
-          EN
-        </a>
-      ),
-    },
-    {
-      key: '2',
-      label: (
-        <a>
-          PL
-        </a>
-      ),
-      disabled: true,
-    },
-  ];
+import plFlag from '../assets/images/plFlag.png';
+import enFlag from '../assets/images/enFlag.png';
 
 export default function LanguageDropdown(){
-    const { i18n } = useTranslation();
+    const { t, i18n } = useTranslation();
     
     const changeLanguageHandler = ({target}) =>
     {
       const {lang} = target;
       i18n.changeLanguage(lang)
     }
+
+    const items = [
+        {
+          key: '1',
+          label: (
+            <a onClick={changeLanguageHandler} lang={"en"} style={{display: 'flex', alignItems: 'baseline'}}>
+                <Image 
+                  width="16px"
+                  preview={false}
+                  src={enFlag} 
+                  style={{marginTop: '-3px'}}
+                />
+              <div style={{marginLeft: '5px'}}>{t('English')}</div>
+            </a>
+          ),
+        },
+        {
+          key: '2',
+          label: (
+            <a onClick={changeLanguageHandler} lang={"pl"} style={{display: 'flex', alignItems: 'baseline'}}>
+                <Image 
+                  width="16px"
+                  preview={false}
+                  src={plFlag} 
+                  style={{marginTop: '-3px'}}
+                />
+              <div style={{marginLeft: '5px'}}>{t('Polish')}</div>
+            </a>
+          )
+        },
+      ];
 
     return (
         <Dropdown
@@ -39,7 +54,7 @@ export default function LanguageDropdown(){
       >
         <a onClick={(e) => e.preventDefault()}>
           <Space>
-            Hover me
+            {t('SelectLanguage')}
             <DownOutlined />
           </Space>
         </a>
