4592ff6b4f46195d0d2d3494b6bbd905b7b1f4ce
Pawel <wilkuwdr2008@gmail.com>
2023-11-17 23:09:08 +0100

    added starting login page css

--- src/routes/login.jsx
+++ src/routes/login.jsx
@@ -4,8 +4,11 @@ import { useState } from "react";
 import axios from 'axios'
 import { useTranslation } from 'react-i18next';
 import LanguageSwitcher from "../components/LanguageSwitcher";
-import { DatePicker } from 'antd';
 
+import { Image } from 'antd';
+import bgUrl from '../assets/images/login_image.jpg';
+import weaiLogo from '../assets/images/weaii-small.png';
+import pskLogo from '../assets/images/polibuda-logo-1.svg';
 
 
 export default function Login() {
@@ -45,14 +48,24 @@ export default function Login() {
       : (
         <div class="login__container">
           <div class="login__container__half">
-            <LanguageSwitcher />
+            <Image 
+              width="86px"
+              preview={false}
+              src={weaiLogo} 
+            />
+          
+            {/* <LanguageSwitcher /> */}
+
+            <div class="login__container__half__wrapper">
             <label style={{display: 'flex', flexDirection: 'column'}}> {t('LoginEmail')}
             <input type="name" onChange={onChange} placeholder={t('LoginEmailPlaceholder')}/>
             </label>
             <button onClick={onSubmit}>{t('Login')}</button>
+            </div>
+
           </div>
-          <div class="login__container__half">
-            
+          
+          <div class="login__container__half" style={{backgroundImage: `url(${bgUrl})`, height: '100%'}}>
           </div>
         </div>
       )
