652e8cc4a1b3a4323277d68dc48e672fa9e7aaf9
Pawel <wilkuwdr2008@gmail.com>
2023-11-18 02:43:51 +0100

    inserted data

--- src/routes/informations.jsx
+++ src/routes/informations.jsx
@@ -16,14 +16,42 @@ export default function Login() {
         <div class="student__informations__container">
             <div class="student__informations__container__half">
                 <h3>Dane studenta</h3>
+                
                 <Divider />
 
                 <h4>Dane z uczelni</h4>
                 
-                <HeadingWithInfo 
-                    title="Numer albumu" 
-                    content={cookies[__cookieName].studentNumber} 
-                />
+                <div class="info_divided_flex_column">
+                    <HeadingWithInfo 
+                        title="Numer albumu" 
+                        content={cookies[__cookieName].studentNumber} 
+                    />
+
+                    <HeadingWithInfo 
+                        title="Kierunek" 
+                        content={cookies[__cookieName].studentProgrammes[0].programme} 
+                    />
+                </div>
+
+                <Divider />
+
+                <h4>Dane osobowe</h4>
+                <div class="info_divided_flex_column">
+                    <HeadingWithInfo 
+                        title="Imię" 
+                        content={cookies[__cookieName].firstName} 
+                    />
+
+                    <HeadingWithInfo 
+                        title="Nazwisko" 
+                        content={cookies[__cookieName].lastName} 
+                    />
+
+                    <HeadingWithInfo 
+                        title="Adres e-mail" 
+                        content={cookies[__cookieName].email} 
+                    />
+                </div>
             </div>
 
             <div class="student__informations__container__half">
@@ -31,6 +59,50 @@ export default function Login() {
                 <Divider />
 
                 <h4>Dane firmy</h4>
+                <div class="info_divided_flex_column">
+                    <HeadingWithInfo 
+                        title="Nazwa firmy" 
+                        content="PLACEHOLDER"
+                    />
+
+                    <HeadingWithInfo 
+                        title="Adres firmy" 
+                        content="ul. PLACEHOLDEROWA 404"
+                    />
+
+                    <HeadingWithInfo 
+                        title="Telefon kontaktowy" 
+                        content="123 456 789"
+                    />
+                </div>
+
+                <Divider />
+                <h4>Dane firmy</h4>
+                <div class="info_divided_flex_column">
+                    <HeadingWithInfo 
+                        title="Miesiąc praktyk" 
+                        content="PLACEHOLDER"
+                    />
+
+                    <HeadingWithInfo 
+                        title="Termin praktyk" 
+                        content="01.07 - 28.07"
+                    />
+                </div>
+
+                <Divider />
+
+                <HeadingWithInfo 
+                        title="Status zaliczenia" 
+                        content="W trakcie"
+                />
+
+                <Divider />
+
+                <HeadingWithInfo 
+                        title="Inna firma" 
+                        content="Posiadasz doświadczenie zawodowe z innej firmy?"
+                />
             </div>
         </div>
     </>
