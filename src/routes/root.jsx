8098bb996dbd85173e746e91c104380994a33ab6
Pawel <wilkuwdr2008@gmail.com>
2023-11-18 12:58:46 +0100

    front page customized to look like something

--- src/routes/root.jsx
+++ src/routes/root.jsx
@@ -2,6 +2,9 @@ import { Outlet, Link, useLoaderData, redirect, useNavigation, Route, Navigate }
 import { useCookies } from 'react-cookie';
 import { useTranslation } from 'react-i18next';
 import Header from "../components/Header";
+import {Divider, Image} from "antd";
+import weaiiLogo from '../assets/images/weaii-big.png';
+import pskLogo from '../assets/images/polibuda-logo-1.svg';
 
 export async function loader({ params }) {
 //   const contacts = await getContacts();
@@ -26,7 +29,34 @@ export default function Root() {
             : (
                 <>
                     <Header />
-                    <h2>{t('Welcome')} {cookies[__cookieName].firstName}!</h2>
+                    <div class="home__container">
+                        <h2>{t('Welcome')} {cookies[__cookieName].firstName} {cookies[__cookieName].lastName}!</h2>
+                        <p>Znajdujesz się właśnie w panelu głównym Systemu Praktyk {cookies[__cookieName].staffStatus > 0 ? "Wykładowcy" : "Studenta"}</p>
+                        <Divider />
+
+                        <h2>Regulamin korzystania z Systemu Praktyk</h2>
+                        <p>
+                        Lorem ipsum dolor sit amet consectetur. Id aliquet ac venenatis lectus tempus fusce. Elit enim neque at justo nisi. Diam nulla dui amet quis lectus in enim risus. Adipiscing nisl interdum ultrices nunc duis non amet vitae. Neque velit elit auctor euismod et rhoncus augue lectus. Odio vitae blandit vitae pharetra in blandit sed tristique. Tristique ipsum nulla id odio tincidunt integer ultricies. Bibendum orci nam habitasse pretium ultricies cursus. Tincidunt felis eget rutrum accumsan. Vitae eget amet pellentesque mi ipsum. Purus ut est dui sit ultricies volutpat massa.
+                        </p>
+                        
+                        <p>
+                        In morbi laoreet ornare euismod id massa quis neque. Non ligula sagittis placerat nunc vel velit amet mattis quis. Vitae eu habitant venenatis facilisis odio volutpat eleifend augue. Integer sapien quam feugiat sapien imperdiet malesuada urna. Consectetur eget lectus urna diam felis diam purus et pretium. Nulla curabitur tristique pharetra sed magna at quam. Varius praesent sit elit hendrerit neque. Laoreet in cras dui gravida. Lacus egestas scelerisque sed pellentesque nunc odio bibendum curabitur. Velit eget ut tristique nam sollicitudin malesuada sapien adipiscing sapien. Risus ipsum egestas risus eget sit sed sit phasellus hendrerit.
+                        </p>
+
+                        <div style={{marginTop:'50px'}}>
+                            <Image 
+                                src={weaiiLogo}
+                                width="350px"
+                                preview={false}
+                            />
+
+                            <Image 
+                                src={pskLogo}
+                                width="200px"
+                                preview={false}
+                            />
+                        </div>
+                    </div>
                 </>
             )
         )
